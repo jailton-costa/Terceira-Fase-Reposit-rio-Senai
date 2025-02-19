cd821ce1296f75b9be0da30fded026d00e41a04e
jailton-costa <jailton.jpc09@gmail.com>
2025-02-18 23:28:22 -0300

    falta pouco para finalizar'

--- story-cubes-digital/src/componentes/Home_StoryCubesDigital.jsx
+++ story-cubes-digital/src/componentes/Home_StoryCubesDigital.jsx
@@ -18,7 +18,8 @@ function Home_StoryCubesDigital() {
 
       {começarJogo && (
         <div style={{
-          padding: "9.6% 14.1%",
+          width: "41%",
+          height: "50%",
           transform: "scale(1)",
           transition: "1s",
           marginTop: "-244px",
@@ -26,22 +27,25 @@ function Home_StoryCubesDigital() {
           border: "1px solid blue",
           borderRadius: "8px",
           position: "absolute",
-          boxShadow: "10px 14px 16px rgba(0, 0, 0, 0.1)"
+          boxShadow: "10px 14px 16px rgba(0, 0, 0, 0.2)"
         }}>
-          <input type="text" className='inputC' placeholder='digite a' />
-          <div className="cardsTudo">
-            <h3 className='h3IMG' >
-              <img src="./img/meme.jpg" className="imgEmojis" />
-            </h3>
-            <h3 className='h3IMG' >
-              <img src="./img/meme.jpg" className="imgEmojis" />
-            </h3>
-            <h3 className='h3IMG' >
-              <img src="./img/meme.jpg" className="imgEmojis" />
-            </h3>
-          </div>
-          <button onClick={() => setComeçarJogo(!começarJogo)} className='btCARD'>voltar para o menu</button>
+          <div className='testoCJ'>
 
+
+            <input type="text" className='inputC' placeholder='como vai acontecer' />
+            <div className="cardsTudo">
+              <h3 className='h3IMG' >
+                <img src="./img/meme.jpg" className="imgEmojis" />
+              </h3>
+              <h3 className='h3IMG' >
+                <img src="./img/meme.jpg" className="imgEmojis" />
+              </h3>
+              <h3 className='h3IMG' >
+                <img src="./img/meme.jpg" className="imgEmojis" />
+              </h3>
+            </div>
+            <button onClick={() => setComeçarJogo(!começarJogo)} className='btCARD'>voltar para o menu</button>
+          </div>
         </div>
       )}
       {ultimoJogo && (
@@ -72,7 +76,8 @@ function Home_StoryCubesDigital() {
       )}
       {comoJogar && (
         <div style={{
-          padding: "7.5% 7.4%",
+          width: "41%",
+          height: "50%",
           transform: "scale(1)",
           transition: "1s",
           marginTop: "-244px",
@@ -82,8 +87,8 @@ function Home_StoryCubesDigital() {
           position: "absolute",
           boxShadow: "10px 14px 16px rgba(0, 0, 0, 0.1)"
         }}>
-          <h3 className='h3Card'>como jogar story-cubes-digital</h3><br />
-          <h4 className='textoCard'>Os Story Cubes (ou Cubo das Histórias) são um conjunto de dados com imagens que ajudam a criar histórias de forma criativa e divertida, É um jogo aberto, ou seja, não tem regras fixas e pode ser jogado de várias maneiras diferentes. Vou te explicar o modo básico de jogar e algumas variações:,story-cubes-digital e um jogo de emojis que os emojis que cair vai decide o que vai acontecer.</h4><br />
+          <h2 className='h3Card'>Como jogar story-cubes-digital</h2><br />
+          <h3 className='textoCard'>Os Story Cubes digital é um conjunto de emojis com imagens que ajudam a criar histórias de forma criativa e divertida, É um jogo aberto, ou seja, não tem regras fixas e pode ser jogado de várias maneiras diferentes.</h3><br />
           <button onClick={() => setComoJogar(!comoJogar)} className='btCARD'>voltar para o menu</button>
 
         </div>
